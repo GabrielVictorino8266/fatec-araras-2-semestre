f0df171b49936d16497b6a75aec89ecaf9048ef5
GabrielVictorino8266 <gabriel.victorino2004@gmail.com>
2024-09-14 08:45:40 -0300

    Estrutura de dados

---
+++ tecnica-programacao-1/self-practice/Temperature.java
@@ -0,0 +1,14 @@
+public class Temperature{
+    private float current_temperature;
+    private float kelvin_temp;
+    private float fahrenheit;
+
+    void showMenu(){
+    }
+
+    void getTemperature(){
+
+    }
+
+
+}
